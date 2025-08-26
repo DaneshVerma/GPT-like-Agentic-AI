f8b091dffd436d34497da9f22d082c4ab75b7abb
Danesh <techydanesh@gmail.com>
2025-08-26 22:58:58 +0530

    socket auth middleware done

--- backend/src/sockets/socket.server.js
+++ backend/src/sockets/socket.server.js
@@ -5,14 +5,14 @@ const userModel = require("../models/user.model");
 function initSocketServer(httpServer) {
   const io = new Server(httpServer);
 
-  io.use((socket, next) => {
-    const cookies = cookie.parse(socket.handshake.headers.cookie || "");
+  io.use(async (socket, next) => {
+    const cookies = cookie.parse(socket.handshake.headers?.cookie || "");
     if (!cookies.token) {
       next(new Error("Authentication erorr: no token provided"));
     }
     try {
       const decoded = jwt.verify(cookies.token, process.env.JWT_SECRET);
-      const user = userModel.findById(decoded.id);
+      const user = await userModel.findById(decoded.id);
       socket.user = user;
       next();
     } catch (error) {
@@ -22,6 +22,7 @@ function initSocketServer(httpServer) {
 
   io.on("connection", (socket) => {
     console.log("new socket connection:", socket.id);
+    console.log("user:", socket.user);
   });
 }
 
